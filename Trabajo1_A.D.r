b8c38a4080e9dbd62a3c6babadf15235e44e21c8
Ignacio Andrés Reynado Cifuentes <ignacioandresreynado@gmail.com>
2026-04-15 12:50:16 -0400

    comentarios

--- Trabajo1_A.D.r
+++ Trabajo1_A.D.r
@@ -1,5 +1,9 @@
+# Ingreso al directorio
 setwd("~/Documentos/GitHub/Trabajo-1-An-lisis-de-Datos")
 
+
+# Importación y limpieza inicial de campos vacíos, ERROR, UNKNOWN a NA
 cafe <- read.csv("dirty_cafe_sales.csv", na.strings = c("", "NA", "UNKNOWN", "ERRROR" ))
 
+# Verificación de campos nulos
 colSums(is.na(cafe))
